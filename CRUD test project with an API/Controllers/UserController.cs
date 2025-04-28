1ee1c4a9c432dbf6ea9c0ae0a4516e1bfacf275b
Koleto <twindestroyersbgbg@gmail.com>
2025-04-28 20:40:21 +0300

    refactor: Moved Error View to UserController
    Deleted Index and Privacy from HomeController

--- CRUD test project with an API/Controllers/UserController.cs
+++ CRUD test project with an API/Controllers/UserController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD_test_project_with_an_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace CRUD_test_project_with_an_API.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger= logger;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
