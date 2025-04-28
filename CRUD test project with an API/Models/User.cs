e2b4fba6be44f86a772ce3dddba1b180184f4e88
Koleto <twindestroyersbgbg@gmail.com>
2025-04-28 20:49:32 +0300

    chore: Added properties for User.cs

--- CRUD test project with an API/Models/User.cs
+++ CRUD test project with an API/Models/User.cs
@@ -1,6 +1,28 @@
-﻿namespace CRUD_test_project_with_an_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_test_project_with_an_API.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string NotUsername { get; set; }
+        [MaxLength(200)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [MaxLength(30)]
+        [Phone]
+        public string Phone {  get; set; }
+        public string Website { get; set; }
+        public string Note { get; set; }
+        public byte IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public User()
+        {
+
+        }
     }
 }
